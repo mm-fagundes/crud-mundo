29c295ebe65a5acb5908b25131d50bd461a354f1
mm-fagundes <mmsf.fagundes@gmail.com>
2025-10-03 21:19:25 -0300

    Estilizacão do formulário
    Estilizacão do formulário

--- web/pags/cadastros/cadastro-paises.php
+++ web/pags/cadastros/cadastro-paises.php
@@ -4,29 +4,32 @@
 
 <body>
     <div class="pg1">
-        <div class="formulario">
-            <form action="../processos/processo-paises.php" method="POST">
-                <div class="box">
-                    <input type="text" name="nome" id="nome" placeholder="Nome do País"   required>
-                </div>    
-               
-                <div class="box">
-                    <input type="number" name="populacao" id="populacao" placeholder="População do País">
-                </div>
+            <div class="formulario">
+                <form action="../processos/processo-paises.php" method="POST">
+                    <div class="box">
+                        <h1>Cadastrar Países</h1>
+                    </div>
+                    <div class="box">
+                        <input type="text" name="nome" id="nome" placeholder="Nome do País"   required>
+                    </div>    
                 
-                <div class="box">
-                    <select id="continente" name="continente" required>
-                        <option value="NA">NA</option>
-                        <option value="SA">SA</option>
-                        <option value="EU">EU</option>
-                        <option value="AS">AS</option>
-                        <option value="AF">AF</option>
-                        <option value="OC">OC</option>
-                    </select>
-                </div>
-                
-                <input type="submit" value="Cadastrar">
-            </form>
-        </div>
+                    <div class="box">
+                        <input type="number" name="populacao" id="populacao" placeholder="População do País">
+                    </div>
+                    
+                    <div class="box">
+                        <select id="continente" name="continente" required>
+                            <option value="NA">North America</option>
+                            <option value="SA">South America</option>
+                            <option value="EU">Europe</option>
+                            <option value="AS">Asia</option>
+                            <option value="AF">Africa</option>
+                            <option value="OC">Oceania</option>
+                        </select>
+                    </div>
+                    
+                    <div class="box"><input type="submit" value="Cadastrar"></div>
+                </form>
+            </div>
     </div>
 </body>
