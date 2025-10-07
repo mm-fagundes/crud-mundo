673429581a885e9eb05bf506ebbe2b885030a08e
Mateus Fagundes <mmsf.fagundes@gmail.com>
2025-10-07 09:09:09 -0300

    Diversas mudanças na index.
    Diversas mudanças na index.
    inserção de busca e etc.

--- web/pags/index.php
+++ web/pags/index.php
@@ -1,5 +1,7 @@
 <?php
-include "navbar.php";
+include 'navbar.php';
+include '../conn/connection.php';
+$escolha = '';
 ?>
 
 
@@ -7,7 +9,44 @@ include "navbar.php";
     <title>Crud Mundo</title>  
 </head>
 
+<body>
+    
+
+<form action="index.php" method="POST">
+    <input type="text" name="pais">
+    <input type="submit" value="Enviar">
+</form>
 
 
-</div>
 
+
+
+
+<?php
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $escolha = $_POST['pais'];
+}
+
+
+
+
+$sql = "select * from paises where nome = '$escolha'";
+$busca = $conn -> query($sql);
+
+if($busca->num_rows > 0){
+    while($linha = $busca->fetch_assoc()){
+        echo "ID: " . $linha['id'] . " - Nome: " . $linha['nome'] . "<br>";
+    }
+}
+
+
+
+
+
+?>
+
+
+
+</body>
