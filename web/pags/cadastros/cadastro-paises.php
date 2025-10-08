2de9fed2f75f34314e07c6b38cff267c5eb004da
jaja-j <mmsf.fagundes@gmail.com>
2025-10-08 09:08:06 -0300

    Algumas mudanças de leve.
    Tentativa de inserção de card(bootstrap) para exibir os dados do país na index.
    
    08/10

--- web/pags/cadastros/cadastro-paises.php
+++ web/pags/cadastros/cadastro-paises.php
@@ -3,43 +3,43 @@
 </head>
 
 <body>
-    <?php include '../navbar.php'; 
+    <?php include '../navbar.php';
     session_start();
-    if(isset($_SESSION['msg'])){
-        echo '<script>alert("' . $_SESSION['msg'] .'")</script>';
+    if (isset($_SESSION['msg'])) {
+        echo '<script>alert("' . $_SESSION['msg'] . '")</script>';
         unset($_SESSION['msg']);
     }
-    
-    
-    
+
+
+
     ?>
     <div class="pg1">
-            <div class="formulario">
-                <form action="../../processos/processo-paises.php" method="POST">
-                    <div class="box">
-                        <h1>Cadastrar Países</h1>
-                    </div>
-                    <div class="box">
-                        <input type="text" name="nome" id="nome" placeholder="Nome do País"   required>
-                    </div>    
-                
-                    <div class="box">
-                        <input type="number" name="populacao" id="populacao" placeholder="População do País">
-                    </div>
-                    
-                    <div class="box">
-                        <select id="continente" name="continente" required>
-                            <option value="NA">North America</option>
-                            <option value="SA">South America</option>
-                            <option value="EU">Europe</option>
-                            <option value="AS">Asia</option>
-                            <option value="AF">Africa</option>
-                            <option value="OC">Oceania</option>
-                        </select>
-                    </div>
-                    
-                    <div class="box"><input type="submit" value="Cadastrar"></div>
-                </form>
-            </div>
+        <div class="formulario">
+            <form action="../../processos/processo-paises.php" method="POST">
+                <div class="box">
+                    <h1>Cadastrar Países</h1>
+                </div>
+                <div class="box">
+                    <input type="text" name="nome" id="nome" placeholder="Nome do País" required>
+                </div>
+
+                <div class="box">
+                    <input type="number" name="populacao" id="populacao" placeholder="População do País">
+                </div>
+
+                <div class="box">
+                    <select id="continente" name="continente" required>
+                        <option value="NA">North America</option>
+                        <option value="SA">South America</option>
+                        <option value="EU">Europe</option>
+                        <option value="AS">Asia</option>
+                        <option value="AF">Africa</option>
+                        <option value="OC">Oceania</option>
+                    </select>
+                </div>
+
+                <div class="box"><input type="submit" value="Cadastrar"></div>
+            </form>
+        </div>
     </div>
 </body>
--- web/pags/index.php
+++ web/pags/index.php
@@ -10,4 +10,22 @@ include '../conn/connection.php';
 
 <body>
 
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <?php
+            if ('REQUEST_METHOD == "POST"') {
+                $sql = "select id, nome, populacao from paises";
+                $result = $conn->query($sql);
+                echo $result;
+            }
+            ?>
+        </div>
+    </div>
+
+
+
+
+
+
 </body>
