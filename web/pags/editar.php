e0efa5e9d72e2c2e84b6b82284ee61a2972f83a2
Mateus Fagundes <mmsf.fagundes@gmail.com>
2025-10-30 09:44:26 -0300

    Atualizacões no editar
    Atualizacões no editar
    - Criação do form de buscar

--- web/pags/editar.php
+++ web/pags/editar.php
@@ -15,33 +15,46 @@ include ('navbar.php');
 include ('links.php');
 ?>
 
-<!-- PARA CONTINUAR IMAGINO QUE DEVO USAR A SECTION PARA PASSAR OS DADOS ATUAIS DA CIDADE Q VEM DO GERENCIAR.PHP -->
-
+<div class="pg1">
+    <div class="formulario" id="pesquisa">
+        <form action="">
+            <div class="box">
+                <h1>Pesquisar Cidade</h1>
+            </div>
+            <div class="box">
+                
+                <input type="text" name="name" placeholder="Nome da Cidade">
+            </div>
+            <div class="box">
+                <input type="submit" value="Buscar">
+            </div>
+        </form>
+    </div>
+</div>
 
 
 <div class="pg1">
-<div class="formulario">
-<form action="../../processos/processo-update-cidades.php" method="post">
-    <div class="box">
-        <h1>Editar Cidades</h1>
-    </div>
-    <div class="box">
-        <input type="text" name="nome" id="nome" placeholder="Nome da Cidade"   required>
-    </div>    
+    <div class="formulario">
+        <form action="../../processos/processo-update-cidades.php" method="post">
+            <div class="box">
+                <h1>Editar Cidades</h1>
+            </div>
+            <div class="box">
+                <input type="text" name="nome" id="nome" placeholder="Nome da Cidade" required disabled>
+            </div>    
                 
-    <div class="box">
-        <input type="number" name="populacao" id="populacao" placeholder="População da Cidade">
-    </div>
+            <div class="box">
+                <input type="number" name="populacao" id="populacao" placeholder="População da Cidade"  required disabled>
+            </div>
     
-    
-    <div class="box">
-        <select id="pais" name="pais" required>
-            <option value="" selected disabled>País</option>
-            <?php while ($pais = $resultado -> fetch_assoc()): ?>
-                <option value="<?= $pais['id'] ?>"><?= $pais['nome'] ?></option>
-            <?php endwhile; ?>
-        </select>
-    </div>
+            <div class="box">
+                <select id="pais" name="pais" required disabled>
+                    <option value="" selected disabled>País</option>
+                    <?php while ($pais = $resultado -> fetch_assoc()): ?>
+                        <option value="<?= $pais['id'] ?>"><?= $pais['nome'] ?></option>
+                    <?php endwhile; ?>
+                </select>
+            </div>
     
     <div class="box"><input type="submit" value="Cadastrar"></div>
 </form>
